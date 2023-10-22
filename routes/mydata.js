5f8e3a17f06d23d6970d3bb6b3ca84fb5f2c9551
ShivaRamReddyMarthala <142836346+ShivaRamReddyMarthala@users.noreply.github.com>
2023-10-22 14:28:08 -0500

    new endpoint

---
+++ routes/mydata.js
@@ -0,0 +1,9 @@
+var express = require('Shiva Ram Reddy Marthala');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
